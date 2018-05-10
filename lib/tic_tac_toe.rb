9e14303dd57f2b723e978f58399153df7ab380d6
Lorena <lorena.giddens@outlook.com>
2018-05-10 20:03:39 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,7 +34,7 @@ class TicTacToe
     @board[index] != " " && @board[index] != ""
   end
   
-  def valid_move?(board, index)
+  def valid_move?(index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 end
